4f1198b05564786d16058652faa37ffc5a6e67fe
jaischeema <jaischeema@gmail.com>
2012-09-13 10:25:35 +1000

    Add the attachment content from proper url

--- lib/roart/attachment.rb
+++ lib/roart/attachment.rb
@@ -11,61 +11,44 @@ module Roart
          if attrs.has_key?(:id) and attrs.has_key?(:ticket)
             self.id = attrs[:id]
             self.ticket = attrs[:ticket]
-            self.load_content
+            self.load_content_type
          end
       end
 
-      def load_content
+      def load_content_type
         uri = self.ticket.class.connection.rest_path + "ticket/#{self.ticket.id}/attachments/#{self.id}"
         page = self.ticket.class.connection.get(uri)
         raise TicketSystemError, "Can't get attachment." unless page
         raise TicketSystemInterfaceError, "Error getting attachment for Ticket: #{self.ticket.id}." unless page.split("\n")[0].include?("200")
-        self.content = get_content_from_page(page)
+        get_content_type_from_page(page)
       end
 
-      def get_content_from_page(page)
+      def content
+        if not @content
+          uri = self.ticket.class.connection.rest_path + "ticket/#{self.ticket.id}/attachments/#{self.id}/content"
+          page = self.ticket.class.connection.get(uri)
+          raise TicketSystemError, "Can't get attachment content." unless page
+          raise TicketSystemInterfaceError, "Error getting attachment content for Ticket: #{self.ticket.id}." unless page.split("\n")[0].include?("200")
+          @content = get_content_from_page(page)
+        end
+        @content
+      end
+
+      def get_content_type_from_page(page)
          page = page.split("\n")
-         collect_lines = false
-         good_lines = []
          page.each do |line|
             if line.match(/^ContentType: (.*)/)
               collections = line.match(/^ContentType: (.*)/)
               self.content_type = collections[1]
-            end
-            if line.match(/^Content: .*/)
-               collect_lines = true
-               collections = line.match(/^Content: (.*)/)
-               good_lines << collections[1]
-               next
-            end
-            if collect_lines
-               collections = line.match(/^\s{9}(.*)/)
-               if collections
-                  good_lines << collections[1]
-               else
-                  pp line
-               end
+              break
             end
          end
-         separator = self.content_type == "text/plain" ? "\n" : " "
-         good_lines.join(separator)
-#         page.delete_if{|x| !x.include?(":") && !x.match(/^ {9}/) && !x.match(/^ {13}/)}
-#         page.each do |ln|
-#            if ln.match(/^ {9}/) && !ln.match(/^ {13}/)
-#               hash[:content] << "\n" + ln.strip if hash[:content]
-#            elsif ln.match(/^ {13}/)
-#               ln = ln.split(":")
-#               hash[:attachments] << "\n" + ln.first.strip if hash[:attachments]
-#            else
-#               ln = ln.split(":")
-#               unless ln.size == 1 || ln.first == 'Ticket' # we don't want to override the ticket method.
-#                  key = ln.delete_at(0).strip.underscore
-#                  value = ln.join(":").strip
-#                  hash[key] = IntKeys.include?(key) ? value.to_i : value
-#               end
-#            end
-#         end
-#         puts hash.inspect
+      end
+      
+      def get_content_from_page(page)
+         page = page.split("\n")
+         2.times { page.shift } #remove the status line and remove the empty line
+         page.join("\n")
       end
    end
 end
